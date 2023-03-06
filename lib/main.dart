516b43484c030d7994a57398a2735f42d6d454d0
Lukaid <lukaid@Lukaidui-MacBookPro-2.local>
2023-03-06 16:27:46 +0900

    :heavy_plus_sign: init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:four_hours_client/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
