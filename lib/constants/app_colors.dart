87c97813885ec7d21e7715be354b108a729dcd12
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-03-17 20:41:44 +0900

    [Add] Add app colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xFFFF8329);
+  static const systemColorRed = Color(0xFFE62319);
+  static const systemColorBlue = Color(0xFF1977E6);
+
+  static const gray50 = Color(0xFFF5F5F5);
+  static const gray100 = Color(0xFFEFEFEF);
+  static const gray200 = Color(0xFFE2E2E2);
+  static const gray300 = Color(0xFFBFBFBF);
+  static const gray400 = Color(0xFFA0A0A0);
+  static const gray500 = Color(0xFF777777);
+  static const gray600 = Color(0xFF636363);
+  static const gray700 = Color(0xFF444444);
+  static const gray800 = Color(0xFF232527);
+  static const gray900 = Color(0xFF121314);
+}
