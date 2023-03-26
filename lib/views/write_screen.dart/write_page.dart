6bdcfa54596cd19984ea51d99066383fa2f125a8
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-03-26 15:31:16 +0900

    [Add] Add fonts

--- lib/views/write_screen.dart/write_page.dart
+++ lib/views/write_screen.dart/write_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:four_hours_client/providers/theme_provider.dart';
+import 'package:four_hours_client/views/widgets/main_wrapper.dart';
 
 class WritePage extends ConsumerWidget {
   const WritePage({Key? key}) : super(key: key);
@@ -9,19 +10,50 @@ class WritePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final isDarkMode = ref.watch(themeNotifierProvider);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        ElevatedButton(
-          onPressed: () =>
-              ref.read(themeNotifierProvider.notifier).changeTheme(),
-          child: const Text('change theme mode'),
-        ),
-        Center(
-          child: Text(isDarkMode ? 'Dark Mode' : 'Light Mode',
-              style: Theme.of(context).textTheme.headlineLarge),
-        )
-      ],
+    return MainWrapper(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () =>
+                ref.read(themeNotifierProvider.notifier).changeTheme(),
+            child: const Text('change theme mode'),
+          ),
+          Center(
+            child: Text(isDarkMode ? 'Dark Mode' : 'Light Mode',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall
+                    ?.copyWith(fontFamily: 'Mont')),
+          ),
+          Center(
+            child: Text(isDarkMode ? 'Dark Mode' : 'Light Mode',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge
+                    ?.copyWith(fontFamily: 'Mont')),
+          ),
+          Center(
+            child: Text(isDarkMode ? 'Dark Mode' : 'Light Mode',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelSmall
+                    ?.copyWith(fontFamily: 'Mont')),
+          ),
+          Center(
+            child: Text(isDarkMode ? '다크 모드' : '라이트 모드',
+                style: Theme.of(context).textTheme.headlineSmall),
+          ),
+          Center(
+            child: Text(isDarkMode ? '다크 모드' : '라이트 모드',
+                style: Theme.of(context).textTheme.titleMedium),
+          ),
+          Center(
+            child: Text(isDarkMode ? '다크 모드' : '라이트 모드',
+                style: Theme.of(context).textTheme.bodyMedium),
+          )
+        ],
+      ),
     );
   }
 }
