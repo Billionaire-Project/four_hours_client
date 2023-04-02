50afc06cc83ffbc2eac281c47e03e512cc562a95
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-04-02 14:27:03 +0900

    [Remove] Remove repositories

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,34 +1,34 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:four_hours_client/repositories/auth_repository.dart';
+import 'package:four_hours_client/services/auth_service.dart';
 
 class AuthProvider extends AsyncNotifier<void> {
   @override
   FutureOr<void> build() {}
 
   Future<void> signInWithGoogle() async {
-    final authRepository = ref.read(authRepositoryProvider);
+    final authService = ref.read(authServiceProvider);
 
     state = const AsyncLoading();
 
-    state = await AsyncValue.guard(authRepository.signInWithGoogle);
+    state = await AsyncValue.guard(authService.signInWithGoogle);
   }
 
   Future<void> signInWithApple() async {
-    final authRepository = ref.read(authRepositoryProvider);
+    final authService = ref.read(authServiceProvider);
 
     state = const AsyncLoading();
 
-    state = await AsyncValue.guard(authRepository.signInWithApple);
+    state = await AsyncValue.guard(authService.signInWithApple);
   }
 
   Future<void> signOut() async {
-    final authRepository = ref.read(authRepositoryProvider);
+    final authService = ref.read(authServiceProvider);
 
     state = const AsyncLoading();
 
-    state = await AsyncValue.guard(authRepository.signOut);
+    state = await AsyncValue.guard(authService.signOut);
   }
 }
 
