5fb253649e926ba38a8b898c6da3e93e0d981d06
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-04-02 20:29:06 +0900

    [Add] Add text style

---
+++ lib/utils/pretendard_text_style.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PretendardTextStyle {
+  static const TextStyle caption = TextStyle(
+    fontFamily: 'Pretendard',
+    fontSize: 13,
+    height: 1.35,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle overline = TextStyle(
+    fontFamily: 'Pretendard',
+    fontSize: 12,
+    height: 1.35,
+    fontWeight: FontWeight.w500,
+  );
+}
