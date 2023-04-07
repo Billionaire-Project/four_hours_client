cc19aec0c6ae5e86d333b4619a55e5ed34befb0c
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-04-07 15:01:15 +0900

    [Add] Add CommonActionSheet

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:four_hours_client/views/widgets/common_action_sheet.dart';
+import 'package:four_hours_client/views/widgets/common_action_sheet_action.dart';
 import 'package:four_hours_client/views/widgets/common_load_dialog.dart';
 
 void showCommonLoadDialog(BuildContext context) {
@@ -11,4 +14,13 @@ void showCommonLoadDialog(BuildContext context) {
 
 void closeCommonLoadDialog(BuildContext context) {
   Navigator.of(context, rootNavigator: true).pop();
+}
+
+void showCommonActionSheet(BuildContext context,
+    {required List<CommonActionSheetAction> actions}) {
+  showCupertinoModalPopup(
+      context: context,
+      builder: (context) {
+        return CommonActionSheet(actions: actions);
+      });
 }
