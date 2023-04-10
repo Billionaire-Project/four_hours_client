bc2234b64a8f02ea4170d168515901e38a1b5980
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-04-11 01:55:24 +0900

    [Remove] Remove Consumer

--- lib/views/home_screen/home_page.dart
+++ lib/views/home_screen/home_page.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:four_hours_client/views/home_screen/bottom_tab_bar.dart';
 import 'package:four_hours_client/views/widgets/main_app_bar.dart';
 import 'package:four_hours_client/views/widgets/main_wrapper.dart';
 
-class HomePage extends ConsumerStatefulWidget {
+class HomePage extends StatefulWidget {
   final Widget child;
 
   const HomePage({Key? key, required this.child}) : super(key: key);
   static const String path = '/home';
 
   @override
-  ConsumerState<HomePage> createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends ConsumerState<HomePage>
+class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   TabController? _tabController;
 
