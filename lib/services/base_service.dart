7022231bed24ccc84fe27c8ef9579188a6773a5f
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-04-16 16:41:08 +0900

    [Add] Add dio

--- lib/services/base_service.dart
+++ lib/services/base_service.dart
@@ -1,7 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:four_hours_client/network/dio_exceptions.dart';
 
 class BaseService {
-  void throwExceptions(Response response) {
+  String throwExceptions(DioError error) {
+    final errorMessage = DioExceptions.fromDioError(error).toString();
+    return errorMessage;
+  }
+
+  void throwError(Response response) {
     var error = response.data['error'];
     if (error != null) {
       throw error;
