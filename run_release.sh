f1fbdb1b1245ab761f3af95c7eb075ba6cedb6b6
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-08-11 00:12:10 +0900

    [Add] Add staging

--- run_release.sh
+++ run_release.sh
@@ -1,3 +1,4 @@
 flutter build apk --flavor staging --target-platform=android-arm64
 
 flutter run --release --flavor production -t lib/main_production.dart 
+flutter run --flavor staging -t lib/main_staging.dart 
