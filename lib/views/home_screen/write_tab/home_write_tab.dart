2e5bd1a78212801fc882e29db81ea5e44964cdf6
JaeHeon-Kim <jaeheon998795@gmail.com>
2023-08-10 23:36:11 +0900

    [Change] topic model까지 받아 왔을 때 로딩을 멈추도록 수정

--- lib/views/home_screen/write_tab/home_write_tab.dart
+++ lib/views/home_screen/write_tab/home_write_tab.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:four_hours_client/constants/constants.dart';
 import 'package:four_hours_client/controller/home_write_controller.dart';
 import 'package:four_hours_client/controller/receipt_controller.dart';
+import 'package:four_hours_client/controller/topic_controller.dart';
+import 'package:four_hours_client/providers/shared_preference_provider.dart';
 import 'package:four_hours_client/views/error_screen/error_page.dart';
 import 'package:four_hours_client/views/home_screen/write_tab/home_write_my_posts.dart';
 import 'package:four_hours_client/views/home_screen/write_tab/home_write_skeleton.dart';
@@ -20,27 +23,44 @@ class HomeWriteTab extends ConsumerWidget {
     final myPosts = ref.watch(homeWriteControllerProvider);
     final myPostsNotifier = ref.read(homeWriteControllerProvider.notifier);
     final asyncReceipt = ref.watch(receiptControllerProvider);
+    final sharedPreferences = ref.watch(sharedPreferencesProvider);
 
     return myPosts.when(
       data: (posts) {
-        return SmartRefresher(
-          physics: const BouncingScrollPhysics(),
-          enablePullDown: true,
-          enablePullUp: true,
-          controller: myPostsNotifier.refreshController,
-          scrollController: myPostsNotifier.scrollController,
-          onRefresh: myPostsNotifier.refreshTab,
-          header: const CustomRefresherHeader(),
-          footer: const CustomRefresherFooter(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Today(asyncReceipt: asyncReceipt),
-              const Gap(16),
-              const MyPosts(),
-            ],
-          ),
+        final asyncTopicModel = ref.watch(topicControllerProvider);
+
+        return asyncTopicModel.when(
+          data: (topicModel) {
+            return SmartRefresher(
+              physics: const BouncingScrollPhysics(),
+              enablePullDown: true,
+              enablePullUp: true,
+              controller: myPostsNotifier.refreshController,
+              scrollController: myPostsNotifier.scrollController,
+              onRefresh: myPostsNotifier.refreshTab,
+              header: const CustomRefresherHeader(),
+              footer: const CustomRefresherFooter(),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ElevatedButton(
+                      onPressed: () async {
+                        await sharedPreferences.setBool(
+                          SharedPreferenceKey.onboarding,
+                          false,
+                        );
+                      },
+                      child: const Text('is shown onboarding false')),
+                  Today(asyncReceipt: asyncReceipt),
+                  const Gap(16),
+                  const MyPosts(),
+                ],
+              ),
+            );
+          },
+          loading: () => const HomeWriteSkeleton(),
+          error: (error, _) => ErrorPage(error: error),
         );
       },
       loading: () => const HomeWriteSkeleton(),
